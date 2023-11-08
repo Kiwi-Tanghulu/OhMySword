ef161e9bdcb82c28cdeddc41208f81298b18a9d7
kjs613 <ggm.kimjiseong613@gmail.com>
2023-11-08 11:21:42 +0900

    Connect PlayerWeapon

--- OhMySword_Client/Assets/01. Scripts/Weapon/PlayerWeapon.cs
+++ OhMySword_Client/Assets/01. Scripts/Weapon/PlayerWeapon.cs
@@ -5,29 +5,28 @@ using UnityEngine;
 public class PlayerWeapon : MonoBehaviour
 {
     [Range(0.001f,0.05f)]
-    [SerializeField] private float scoreSize;
+    [SerializeField] private float scoreSize; // 스코어당 길어지는 길이! 예) 0.01이라면 1점당 0.01이 커짐 100점이라면 1이 커지고
 
     [SerializeField] private Transform swordPivot;
-    private int swordSize;
-    private int currentScore = 0;
-    private int nextScore = 0;
+    private int swordSize; // 나중에 필요할 수 있어서...
+    public int SwordSize => swordSize;
+
+    private int currentSwordLevel = 0;
+    public int CurrentSwordLevel => currentSwordLevel;
+
+    private ushort currentScore = 0;
+    private ushort nextScore = 0;
     private bool isGrowing = false;
-    private void Update()
+    
+    public void SetSwordSize() //나중에 특정 구간에서 칼이 바뀐다면 채워넣어야함
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TestGetScore((int)Random.Range(1, 20));
-        }
+
     }
-
-    private void TestGetScore(int value)
+    public void SetScore(ushort value)
     {
-        nextScore += value;
+        nextScore = value;
         if(currentScore != nextScore && isGrowing == false)
-        {
             StartCoroutine(GrowUpSword());
-        }
-
     }
 
     private IEnumerator GrowUpSword()
