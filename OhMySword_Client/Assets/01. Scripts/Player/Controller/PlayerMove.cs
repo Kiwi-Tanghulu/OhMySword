76b078c3cc53b7c2eb2e9fe4ba33432dc46984e2
SEH00N <06psh0501@gmail.com>
2023-11-09 21:00:43 +0900

    드가자

--- OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerMove.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerMove.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEditor.PlayerSettings;
 
 public class PlayerMove : MonoBehaviour
 {
