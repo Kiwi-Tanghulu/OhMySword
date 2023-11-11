bcab06b45adfa30904f9e89ea0035682d4956988
JUNSUNG06 <forest2847@gmail.com>
2023-11-11 19:58:43 +0900

    .

--- OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerInput.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerInput.cs
@@ -6,11 +6,13 @@ using UnityEngine.Events;
 public class PlayerInput : MonoBehaviour
 {
     public UnityEvent<Vector3> Movement;
+    public UnityEvent<Vector3> Mouse;
     public UnityEvent LeftClick;
 
     private void Update()
     {
         LeftClick?.Invoke();
         Movement.Invoke(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
+        Mouse.Invoke(new Vector3(Input.GetAxisRaw("MouseX"), Input.GetAxisRaw("MouseY")));
     }
 }
