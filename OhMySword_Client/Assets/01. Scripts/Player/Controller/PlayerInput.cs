eaac97776c79215bfbd5e220eaa39d90e81c39ff
JUNSUNG06 <forest2847@gmail.com>
2023-11-11 20:29:39 +0900

    player view

--- OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerInput.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerInput.cs
@@ -6,13 +6,13 @@ using UnityEngine.Events;
 public class PlayerInput : MonoBehaviour
 {
     public UnityEvent<Vector3> Movement;
-    public UnityEvent<Vector3> Mouse;
+    public UnityEvent<Vector2> Mouse;
     public UnityEvent LeftClick;
 
     private void Update()
     {
         LeftClick?.Invoke();
-        Movement.Invoke(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
-        Mouse.Invoke(new Vector3(Input.GetAxisRaw("MouseX"), Input.GetAxisRaw("MouseY")));
+        Movement?.Invoke(new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")));
+        Mouse?.Invoke(new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")));
     }
 }
--- OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerMove.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerMove.cs
@@ -1,43 +1,88 @@
+using System.IO.Compression;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMove : MonoBehaviour
 {
     private ActiveRagdoll ragdoll;
 
-    public bool canMove = true;
+    [SerializeField] UnityEvent<Vector3> onMovedEvent;
 
+    [SerializeField] private Rigidbody hip;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private bool canMove = true;
     private Vector3 prevTargetPos;
     private Vector3 targetPos;
     private Vector3 moveDir;
+    private Vector3 prevMoveDir;
     private Vector3 velocity;
-    [SerializeField] private float moveSpeed;
+    private float moveDistance;
+
+    private PlayerView cam;
 
     private void Start()
     {
         ragdoll = GetComponent<ActiveRagdoll>();
+        cam = GetComponent<PlayerView>();
         targetPos = transform.position;
         prevTargetPos = targetPos;
     }
 
-    public void Move(Vector3 movedir)
+    #region MYMOVE
+    public void SetMoveDirection(Vector3 input)
     {
-        if(canMove)
-            ragdoll.SetVelocity(movedir * moveSpeed);
+        prevMoveDir = moveDir;
+        moveDir = cam.forward * input.z + cam.right * input.x;
+    }
+
+    public void Move()
+    {
+        if ((targetPos - hip.transform.position).magnitude > 0.5f)
+            ragdoll.SetVelocity(moveDir * moveSpeed);
         else
             ragdoll.SetVelocity(Vector3.zero);
-
+        onMovedEvent?.Invoke(hip.transform.position);
     }
+    #endregion
 
+    #region OTHER MOVE
     public void SetTargetPosition(Vector3 pos)
     {
-        prevTargetPos = targetPos;
-        transform.position = prevTargetPos;
-        targetPos = pos;
-        moveDir = (targetPos - prevTargetPos).normalized;
-        velocity = moveDir * moveSpeed;
+        if (Vector3.Distance(pos, hip.transform.position) < 0.1f)
+            moveDir = Vector3.zero;
+        else
+        {
+            prevTargetPos = targetPos;
+            targetPos = pos;
+            moveDir = targetPos - hip.transform.position;
+        }
+
+        moveDistance = moveDir.magnitude;
+        moveDir.Normalize();
+
+        SetVelocity();
     }
+
+    private void SetVelocity()
+    {
+        moveSpeed = moveDistance / 0.1f;
+        ragdoll.SetVelocity(moveDir * moveSpeed);
+
+        StartCoroutine(AdjustPosition());
+    }
+
+    private IEnumerator AdjustPosition()
+    {
+        yield return new WaitForSeconds(0.09f);
+
+
+        if (Vector3.Distance(targetPos, hip.transform.position) > 0.5f)
+            hip.transform.position = targetPos;
+    }
+    #endregion
 
     public void Stun(float time)
     {
