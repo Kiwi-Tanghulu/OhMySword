4372f96cd372e595236156ea57247924ae260cea
SEH00N <06psh0501@gmail.com>
2023-11-13 23:26:02 +0900

    ui audio

---
+++ OhMySword_Client/Assets/01. Scripts/System/Sound/PlayAudioCallback.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayAudioCallback : MonoBehaviour
+{
+    [SerializeField] string clipName;
+
+	public void PlayAudio(string clipName)
+    {
+    }
+
+    public void PlayOneShot(string clipName)
+    {
+
+    }
+
+    public void PlayAudio()
+    {
+
+    }
+
+    public void PlayOneShot()
+    {
+
+    }
+}
