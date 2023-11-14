1c093d4dcf410bcd33387e996f4582000b961580
JUNSUNG06 <forest2847@gmail.com>
2023-11-14 12:22:58 +0900

    modify cam

--- OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerView.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/Controller/PlayerView.cs
@@ -12,6 +12,7 @@ public class PlayerView : MonoBehaviour
     [SerializeField] private float minRotate;
     [SerializeField] private Transform target;
     [SerializeField] private Transform hip;
+    [SerializeField] private Transform camAnchor;
 
     private Vector2 rotation = Vector2.zero;
     private Vector3 targetRotation = Vector3.zero;
@@ -25,7 +26,8 @@ public class PlayerView : MonoBehaviour
         rotation.x = Mathf.Clamp(rotation.x, minRotate, maxRotate);
 
         target.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
-        hip.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        camAnchor.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        hip.rotation = Quaternion.Euler(0, rotation.y, 0);
 
         onRotatedEvent?.Invoke(hip.eulerAngles);
     }
@@ -36,7 +38,8 @@ public class PlayerView : MonoBehaviour
         targetRotation = euler;
 
         StartCoroutine(RotateCoroutine(target, Quaternion.Euler(prevRotation.x, prevRotation.y, 0), Quaternion.Euler(targetRotation.x, targetRotation.y, 0)));
-        StartCoroutine(RotateCoroutine(hip, Quaternion.Euler(prevRotation.x, prevRotation.y, 0), Quaternion.Euler(targetRotation.x, targetRotation.y, 0)));
+        StartCoroutine(RotateCoroutine(camAnchor, Quaternion.Euler(prevRotation.x, prevRotation.y, 0), Quaternion.Euler(targetRotation.x, targetRotation.y, 0)));
+        StartCoroutine(RotateCoroutine(hip, Quaternion.Euler(0, prevRotation.y, 0), Quaternion.Euler(0, targetRotation.y, 0)));
     }
 
     private IEnumerator RotateCoroutine(Transform trm, Quaternion from, Quaternion target, float duration = 0.09f)
