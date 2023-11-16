59c9ca97f503ad10613250875c248cbb6c38167b
JUNSUNG06 <forest2847@gmail.com>
2023-11-17 01:28:25 +0900

    modify scorebox attack effeect

--- OhMySword_Client/Assets/01. Scripts/Player/PlayerController.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/PlayerController.cs
@@ -12,6 +12,7 @@ namespace OhMySword.Player
         private PlayerMove movement;
         private PlayerView view;
         private PlayerWeapon playerWeapon;
+        public ActiveRagdoll ragdoll { get; private set; }
 
         public UnityEvent<SyncableObject> OnHitEvent;
         public UnityEvent<SyncableObject> OnDieEvent;
@@ -32,6 +33,7 @@ namespace OhMySword.Player
             movement = GetComponent<PlayerMove>();
             view = GetComponent<PlayerView>();
             playerWeapon = transform.Find("Hips/Rig/Sword/Sword").GetComponent<PlayerWeapon>();
+            ragdoll = GetComponent<ActiveRagdoll>();
 
             audioPlayer = GetComponent<AudioSource>();
         }
