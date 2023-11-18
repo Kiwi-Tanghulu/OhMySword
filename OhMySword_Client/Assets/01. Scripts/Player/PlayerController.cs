4e4bb4684af1ee97bf9f0c2c4ca1f5246fb98782
SEH00N <06psh0501@gmail.com>
2023-11-18 23:47:16 +0900

    player die todo

--- OhMySword_Client/Assets/01. Scripts/Player/PlayerController.cs
+++ OhMySword_Client/Assets/01. Scripts/Player/PlayerController.cs
@@ -76,6 +76,17 @@ namespace OhMySword.Player
             Debug.Log($"die : {transform.name}");
             OnDieEvent?.Invoke(attacker);
             AudioManager.Instance.PlayAudio("PlayerDie", audioPlayer, true);
+
+            // 콜라이더 끄기
+
+            if(ObjectID == RoomManager.Instance.PlayerID) // 나 자신
+            {
+                // 내가 움직이는 걸 서버에 보내지 않아야 함
+            }
+            else // 다른 사람
+            {
+                // 서버에서 오는 데이터로부터 해당 오브젝트에 어떠한 동기화도 해주면 안 됨 
+            }
         }
 
         public void DoChat(string chat)
