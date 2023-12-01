95f9f0177079742dede9e28bd7aa057ed337b408
JUNSUNG06 <forest2847@gmail.com>
2023-12-02 03:07:34 +0900

    chicken text, modify chat activity

--- OhMySword_Client/Assets/01. Scripts/Event/Chicken/ChickenEvent.cs
+++ OhMySword_Client/Assets/01. Scripts/Event/Chicken/ChickenEvent.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class ChickenEvent : GameEvent
 {
     private GameObject chickenResource;
     private Chicken chicken;
+    private TextMeshProUGUI chickenText;
 
     public override void InitEvent()
     {
         base.InitEvent();
         chickenResource = Resources.Load<GameObject>("Prefabs/Chicken");
+        chickenText = GameObject.Find("MainCanvas/ChickenText").GetComponent<TextMeshProUGUI>();
     }
 
     public override void StartEvent()
@@ -18,6 +22,7 @@ public class ChickenEvent : GameEvent
         Debug.Log("Start Chicken Event");
         chicken = GameObject.Instantiate(chickenResource).GetComponent<Chicken>();
         RoomManager.Instance.ObjectParent.gameObject.SetActive(false);
+        chickenText.transform.DOScale(0.75f, 1.2f).SetEase(Ease.InOutBack);
     }
 
     public override void UpdateEvent()
@@ -31,5 +36,6 @@ public class ChickenEvent : GameEvent
         GameObject.Destroy(chicken.gameObject);
         chicken = null;
         RoomManager.Instance.ObjectParent.gameObject.SetActive(true);
+        chickenText.transform.DOScale(0f, 1.2f).SetEase(Ease.InOutBack);
     }
 }
